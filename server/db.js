a00c40baa5720e31f3728a4b2d6a328e7fe86ffb
unknown <dimapen57@gmail.com>
2024-02-26 19:08:34 +0000

    changesSequlize

--- server/db.js
+++ server/db.js
@@ -16,14 +16,22 @@ pool.connect((err) => {
   console.log("Connected to PostgreSQL");
 });
 // for local at laptop
+// export const sequelize = new Sequelize({
+//   dialect: "postgres",
+//   host: process.env.DB_HOST,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+//   logging: console.log,
+// });
 export const sequelize = new Sequelize({
   dialect: "postgres",
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.POSTGRES_HOST, // Use Vercel environment variable
+  username: process.env.POSTGRES_USER, // Use Vercel environment variable
+  password: process.env.POSTGRES_PASSWORD, // Use Vercel environment variable
+  database: process.env.POSTGRES_DB, // Use Vercel environment variable
+  port: process.env.POSTGRES_PORT, // Use Vercel environment variable
   logging: console.log,
 });
-
 // ... rest of the Sequelize configuration
