e3f25268096164f63741f4b9aea49ee392f90cc7
unknown <dimapen57@gmail.com>
2024-02-26 23:13:41 +0000

    fixLoader

--- client/src/components/ReactSkeleton/Skeletom.jsx
+++ client/src/components/ReactSkeleton/Skeletom.jsx
@@ -4,15 +4,26 @@ import ContentLoader from "react-content-loader";
 const MyLoader = (props) => (
   <ContentLoader
     speed={2}
-    width={200}
+    width="100%" // Adjusted width to 100%
     height={413}
-    viewBox="0 0 200 413"
+    viewBox="100%" // Adjusted viewBox to maintain the aspect ratio
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
     {...props}
   >
-    <rect x="-6" y="0" rx="0" ry="0" width="200" height="413" />
+    <rect x="-6" y="0" rx="0" ry="0" width="100%" height="413" />
   </ContentLoader>
 );
 
 export default MyLoader;
+{
+  /* <ContentLoader
+speed={2}
+width={200}
+height={413}
+viewBox="0 0 200 413"
+backgroundColor="#f3f3f3"
+foregroundColor="#ecebeb"
+{...props}
+> */
+}
